3efa0a83978a45b27a058218f79540882d6b26bf
halieheacock <haliebug1108@gmail.com>
2018-07-27 19:36:57 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,14 +1,27 @@
-class AmazonTransaction
-  attr_accessor :total, :items, :quantity, :title, :price, :discount
-def initialize(total=0, discount = 20)
-  @total = total
-  @items = []
-  @discount = discount
+class User 
+  attr_reader :username, :password, :type
+  attr_writer :username, :password, :type
+  
+  def initialize(username, password, type)
+    @username = username
+    @password = password
+    @type = type 
+  end 
+  
+  def like(surfer)
+    puts "#{surfer} just liked your post!"
+  end 
+  
+  def comment(surfer, comment)
+    puts "#{surfer} just commented ''#{comment}'' on your post"
   end
   
-  def add_item(title, price, quantity = 1)
-    price *= quantity
-    @total += price
-  @items << title
-  end
-end
+end 
+
+elaine = User.new("Elaine", "password123", "public")
+caroline = User.new("cdeocampo", "yalaci2001", "private")
+andrew = User.new("Andrew", "yalaai1985", "public")
+
+puts "What is your username, surfer?"
+puts "What is your comment, surfer?"
+puts elaine.comment(gets.strip, gets.strip)
